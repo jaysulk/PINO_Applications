fa15d952b174d6f6333ec7d154af9c771b836a01
Jason Sulskis <Sulskis_Jason@bah.com>
2023-02-13 12:59:45 -0800

    Update basics.py
    Added new definitions of DHT.

--- models/basics.py
+++ models/basics.py
@@ -5,24 +5,67 @@ import torch.nn as nn
 
 from functools import partial
 
-def DiscreteHartleyTransform(input):
-	fft = torch.rfft(input, 2, normalized=True, onesided=False)
-	dht = fft[:, :, :, :, -2] - fft[:, :, :, :, -1]
+def DiscreteHartleyTransform(X:torch.Tensor):
+	fft = torch.fft.fft(X)
+	dht = torch.real(fft) - torch.imag(fft)
 	return dht
+
+def InverseDiscreteHartleyTransform(X:torch.Tensor):
+    """ Compute the IDHT for a sequence x of length n using the FFT. 
+    
+    Since the DHT is involutory, IDHT(x) = 1/n DHT(H) = 1/n DHT(DHT(x))
+    """
+    n = len(X)
+    x = DiscreteHartleyTransform(X)
+    x = 1.0/n*x
+    return x
+
+def flip_periodic(x:torch.Tensor):
+    return torch.roll(torch.flip(x,dims=[0]),1)
+
+def dht_conv(x:torch.Tensor, y:torch.Tensor):
+    """ Computes the DHT of the convolution of x and y, sequences of length n, using FFT.
+    This is a straightforward implementation of the convolution theorem for the
+    DHT. See https://en.wikipedia.org/wiki/Discrete_Hartley_transform#Properties
+    """  
+    
+    err_msg = "x and y must be 1D sequences of the same length"
+    assert (x.shape == y.shape) & (torch.squeeze(x).ndim < 2), err_msg
+
+    X = DiscreteHartleyTransform(x)
+    Y = DiscreteHartleyTransform(y)
+    Xflip = flip_periodic(X)
+    Yflip = flip_periodic(Y)
+    Yeven = 0.5 * (Y + Yflip)
+    Yodd  = 0.5 * (Y - Yflip)
+    Z = X * Yeven + Xflip * Yodd
+    return Z    
+
+
+def conv(x:torch.Tensor, y:torch.Tensor):
+    """ Computes the convolution of x and y, sequences of length n, using the DHT.
+    Once the DHT of the convolution has benn computed using dht_conv(), 
+    computing the convolution just requires a IDHT.
+    """  
+    
+    err_msg = "x and y must be 1D sequences of the same length"
+    assert (x.shape == y.shape) & (np.squeeze(x).ndim < 2), err_msg
+
+    Z = dht_conv(x, y)
+    z = InverseDiscreteHartleyTransform(Z)
+    return z
 
 def compl_mul1d(a, b):
     # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
-    return 0.5*(torch.einsum("bix,iox->box", a, b) + torch.einsum("bix,iox->box", a, torch.mul(b,-1)))+0.5*(torch.einsum("bix,iox->box", torch.mul(a,-1), b) + torch.einsum("bix,iox->box", a, torch.mul(b,-1)))
-
+    return conv(a, b) 
 
 def compl_mul2d(a, b):
     # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
-    return 0.5*(torch.einsum("bixy,ioxy->boxy", a, b) + torch.einsum("bixy,ioxy->boxy", a, torch.mul(b,-1)))+0.5*(torch.einsum("bixy,ioxy->boxy", torch.mul(a,-1), b) + torch.einsum("bixy,ioxy->boxy", a, torch.mul(b,-1)))
-
+    return conv(a, b) 
 
 def compl_mul3d(a, b):
-    return 0.5*(torch.einsum("bixyz,ioxyz->boxyz", a, b) + torch.einsum("bixyz,ioxyz->boxyz", a, torch.mul(b,-1)))+0.5*(torch.einsum("bixyz,ioxyz->boxyz", torch.mul(a,-1), b) + torch.einsum("bixyz,ioxyz->boxyz", a, torch.mul(b,-1)))
-
+    return conv(a, b) 
+    
 ################################################################
 # 1d fourier layer
 ################################################################
