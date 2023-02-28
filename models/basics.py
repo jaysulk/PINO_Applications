8dce3b1195818d704afd3a73c6bac0cfd416afe5
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2023-02-27 18:43:00 -0800

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from functools import partial
 
 def DiscreteHartleyTransform(X:torch.Tensor,s,dim):
-	fft = torch.fft.rfftn(X, s=s, dim=dim)
+	fft = torch.fft.rfftn(X, s=s, dim=dim, normalized=True, onesided=False)
 	return torch.real(fft) - torch.imag(fft)
 
 def InverseDiscreteHartleyTransform(X:torch.Tensor, s, dim):
