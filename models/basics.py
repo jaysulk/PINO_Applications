1de1eba67e20d45a5c7e170410bfc3f1e83a5449
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2023-08-05 16:54:41 -0700

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -60,27 +60,23 @@ class SpectralConv1d(nn.Module):
 
         self.scale = (1 / (in_channels*out_channels))
         self.weights1 = nn.Parameter(
-            self.scale * torch.rand(in_channels, out_channels, self.modes1))
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, 2))
 
     def forward(self, x):
         batchsize = x.shape[0]
-        x = x.detach().cpu().numpy()
+        # Compute Hartley coefficients up to factor of h^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2])
+        x_ft_mirror = torch.fft.rfftn(x.flip(dims=[2]), dim=[2])  # F(-u)
+        x_ht = x_ft + x_ft_mirror
 
-        # Compute Hartley coeffcients
-        x_ht = np.fft.hfft(x, axis=2)
+        # Multiply relevant Hartley modes
+        out_ht = torch.zeros(batchsize, self.in_channels, x.size(-1)//2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ht[:, :, :self.modes1] = compl_mul1d(x_ht[:, :, :self.modes1], self.weights1)
 
-        # Convert numpy array to torch tensor and multiply relevant Hartley modes
-        x_ht = torch.from_numpy(x_ht).to(x.device)
-        out_ht = torch.zeros(batchsize, self.in_channels, x.shape[-1]//2 + 1, device=x.device)
-        out_ht[:, :, :self.modes1] = x_ht[:, :, :self.modes1] * self.weights1
-
-        # Convert torch tensor to numpy array for inverse Hartley transform
-        out_ht = out_ht.detach().cpu().numpy()
-        
         # Return to physical space
-        x = np.fft.ihfft(out_ht, axis=2)
+        x = torch.fft.irfft(out_ht, s=[x.size(-1)], dim=[2])
+        return x
 
-        return torch.from_numpy(x).to(out_ht.device)
 
 
 ################################################################
@@ -90,102 +86,86 @@ class SpectralConv1d(nn.Module):
 class SpectralConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, modes1, modes2):
         super(SpectralConv2d, self).__init__()
-
-        """
-        2D Hartley layer. It does HHT, linear transform, and Inverse HHT.    
-        """
-
         self.in_channels = in_channels
         self.out_channels = out_channels
         # Number of Hartley modes to multiply, at most floor(N/2) + 1
         self.modes1 = modes1
         self.modes2 = modes2
 
-        self.scale = (1 / (in_channels*out_channels))
+        self.scale = (1 / (in_channels * out_channels))
         self.weights1 = nn.Parameter(
-            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2))
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
 
     def forward(self, x):
         batchsize = x.shape[0]
-        x = x.detach().cpu().numpy()
+        # Compute Hartley coeffcients up to factor of h^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2, 3])
+        x_ft_mirror = torch.fft.rfftn(x.flip(dims=[2, 3]), dim=[2, 3])  # F(-u)
+        x_ht = x_ft + x_ft_mirror
 
-        # Compute Hartley coefficients
-        x_ht = np.fft.hfft(x, axes=[2, 3])
-
-        # Convert numpy array to torch tensor and multiply relevant Hartley modes
-        x_ht = torch.from_numpy(x_ht).to(x.device)
-        out_ht = torch.zeros(batchsize, self.out_channels, x.shape[-2], x.shape[-1]//2 + 1, device=x.device)
-        out_ht[:, :, :self.modes1, :self.modes2] = x_ht[:, :, :self.modes1, :self.modes2] * self.weights1
-
-        # Convert torch tensor to numpy array for inverse Hartley transform
-        out_ht = out_ht.detach().cpu().numpy()
+        # Multiply relevant Hartley modes
+        out_ht = torch.zeros(batchsize, self.out_channels, x.size(-2), x.size(-1) // 2 + 1, device=x.device,
+                             dtype=torch.cfloat)
+        out_ht[:, :, :self.modes1, :self.modes2] = \
+            compl_mul2d(x_ht[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ht[:, :, -self.modes1:, :self.modes2] = \
+            compl_mul2d(x_ht[:, :, -self.modes1:, :self.modes2], self.weights2)
 
         # Return to physical space
-        x = np.fft.ihfft(out_ht, axes=[2, 3])
-
-        return torch.from_numpy(x).to(out_ht.device)
+        x = torch.fft.irfftn(out_ht, s=(x.size(-2), x.size(-1)), dim=[2, 3])
+        return x
 
 class SpectralConv3d(nn.Module):
     def __init__(self, in_channels, out_channels, modes1, modes2, modes3):
         super(SpectralConv3d, self).__init__()
-
-        """
-        3D Hartley layer. It does HHT, linear transform, and Inverse HHT.    
-        """
-
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes1 = modes1  #Number of Hartley modes to multiply, at most floor(N/2) + 1
+        self.modes1 = modes1  # Number of Hartley modes to multiply, at most floor(N/2) + 1
         self.modes2 = modes2
         self.modes3 = modes3
 
         self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(
-            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3))
-        
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights3 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights4 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+
     def forward(self, x):
         batchsize = x.shape[0]
-        x = x.detach().cpu().numpy()
+        # Compute Hartley coefficients up to factor of h^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2, 3, 4])
+        x_ft_mirror = torch.fft.rfftn(x.flip(dims=[2, 3, 4]), dim=[2, 3, 4])  # F(-u)
+        x_ht = x_ft + x_ft_mirror
 
-        # Compute Hartley coeffcients
-        x_ht = np.fft.hfft(x, axes=[2, 3, 4])
-
-        # Convert numpy array to torch tensor and multiply relevant Hartley modes
-        x_ht = torch.from_numpy(x_ht).to(x.device)
-        out_ht = torch.zeros(batchsize, self.out_channels, x.shape[2], x.shape[3], x.shape[4]//2 + 1, device=x.device)
+        # Multiply relevant Hartley modes
+        out_ht = torch.zeros(batchsize, self.out_channels, x.size(2), x.size(3), x.size(4)//2 + 1, device=x.device, dtype=torch.cfloat)
         out_ht[:, :, :self.modes1, :self.modes2, :self.modes3] = \
-            x_ht[:, :, :self.modes1, :self.modes2, :self.modes3] * self.weights1
-
-        # Convert torch tensor to numpy array for inverse Hartley transform
-        out_ht = out_ht.detach().cpu().numpy()
+            compl_mul3d(x_ht[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1)
+        out_ht[:, :, -self.modes1:, :self.modes2, :self.modes3] = \
+            compl_mul3d(x_ht[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2)
+        out_ht[:, :, :self.modes1, -self.modes2:, :self.modes3] = \
+            compl_mul3d(x_ht[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3)
+        out_ht[:, :, -self.modes1:, -self.modes2:, :self.modes3] = \
+            compl_mul3d(x_ht[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4)
 
         # Return to physical space
-        x = np.fft.ihfft(out_ht, axes=[2, 3, 4])
+        x = torch.fft.irfftn(out_ht, s=(x.size(2), x.size(3), x.size(4)), dim=[2, 3, 4])
+        return x
 
-        return torch.from_numpy(x).to(out_ht.device)
-
-
-import torch
-import torch.nn as nn
 
 class FourierBlock(nn.Module):
     def __init__(self, in_channels, out_channels, modes1, modes2, modes3, activation='tanh'):
         super(FourierBlock, self).__init__()
-
-        """
-        Fourier Block is a transformation block consisting of spectral convolution, a linear transformation,
-        and a non-linear activation function.
-        """
-
         self.in_channel = in_channels
         self.out_channel = out_channels
         self.speconv = SpectralConv3d(in_channels, out_channels, modes1, modes2, modes3)
-        self.linear = nn.Linear(in_channels, out_channels)
-
+        self.linear = nn.Conv1d(in_channels, out_channels, 1)
         if activation == 'tanh':
-            self.activation = torch.tanh
+            self.activation = torch.tanh_
         elif activation == 'gelu':
-            self.activation = nn.GELU()
+            self.activation = nn.GELU
         elif activation == 'none':
             self.activation = None
         else:
@@ -198,7 +178,7 @@ class FourierBlock(nn.Module):
         x1 = self.speconv(x)
         x2 = self.linear(x.view(x.shape[0], self.in_channel, -1))
         out = x1 + x2.view(x.shape[0], self.out_channel, x.shape[2], x.shape[3], x.shape[4])
-
         if self.activation is not None:
             out = self.activation(out)
         return out
+
