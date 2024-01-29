09107e033c164e8bec8e6665c35b60c99ceef17c
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-01-28 18:07:11 -0800

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -21,7 +21,7 @@ def dht(x: torch.Tensor):
 
     return H
 
-def idht(x):
+def idht(x: torch.Tensor):
     # Assume that dht2d is already defined for NumPy
     # Get the dimensions of X
     dims = x.shape
