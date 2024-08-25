4cf5d4f0185ec4f7f35f6570c720bf2942deb938
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-08-25 17:02:02 -0400

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -12,7 +12,7 @@ import torch
 
 import torch
 
-def dht(x: torch.Tensor):
+def dht(x: torch.Tensor) -> torch.Tensor:
     # Compute the 2D FFT
     fft = torch.fft.fft(x, norm="ortho")
 
@@ -21,36 +21,57 @@ def dht(x: torch.Tensor):
 
     return H
 
-def idht(x: torch.Tensor):
+def idht(x: torch.Tensor) -> torch.Tensor:
     dims = x.size()
     n = torch.prod(torch.tensor(dims)).item()
     X = dht(x)
     H = X / n
     return H
 
-def compl_mul1d(a, b):
-    # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
-    a_fft = dht(a)
-    b_fft = dht(b)
-    result_fft =torch.einsum("bix,iox->box", a, b)
-    result = idht(result_fft)
+def compl_mul1d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+    # Compute the DHT of both signals
+    X1_H_k = x1
+    X2_H_k = x2
+    X1_H_neg_k = x1.flip(0)
+    X2_H_neg_k = x2.flip(0)
+    N = x1.size(0)
+
+    result = 0.5 * (torch.einsum('bix,iox->box', X1_H_k, X2_H_k) - 
+                     torch.einsum('bix,iox->box', X1_H_neg_k, X2_H_neg_k) +
+                     torch.einsum('bix,iox->box', X1_H_k, X2_H_neg_k) + 
+                     torch.einsum('bix,iox->box', X1_H_neg_k, X2_H_k))
+
     return result
 
+def compl_mul2d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+    # Compute the DHT of both signals
+    X1_H_k = x1
+    X2_H_k = x2
+    X1_H_neg_k = x1.flip(0)
+    X2_H_neg_k = x2.flip(0)
+    N = x1.size(0)
 
-def compl_mul2d(a, b):
-    # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
-    a_fft = dht(a)
-    b_fft = dht(b)
-    result_fft = torch.einsum("bixy,ioxy->boxy", a, b)
-    result = idht(result_fft)
+    result = 0.5 * (torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_k) - 
+                     torch.einsum('bixy,ioxy->boxy', X1_H_neg_k, X2_H_neg_k) +
+                     torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_neg_k) + 
+                     torch.einsum('bixy,ioxy->boxy', X1_H_neg_k, X2_H_k))
+
     return result
     
-def compl_mul3d(a, b):
-    a_fft = dht(a)
-    b_fft = dht(b)
-    result_fft = torch.einsum("bixyz,ioxyz->boxyz", a, b)
-    result = idht(result_fft)
-    return result.real
+def compl_mul3d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+    # Compute the DHT of both signals
+    X1_H_k = x1
+    X2_H_k = x2
+    X1_H_neg_k = x1.flip(0)
+    X2_H_neg_k = x2.flip(0)
+    N = x1.size(0)
+
+    result = 0.5 * (torch.einsum('bixyz,ioxyz->boxyz', X1_H_k, X2_H_k) - 
+                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_neg_k, X2_H_neg_k) +
+                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_k, X2_H_neg_k) + 
+                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_neg_k, X2_H_k))
+
+    return result
 
 ################################################################
 # 1d fourier layer
