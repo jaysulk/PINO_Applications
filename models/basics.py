01bc3dd9c61764066f5a2a113914b5699c101784
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-08-25 17:20:55 -0400

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -32,8 +32,8 @@ def compl_mul1d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     # Compute the DHT of both signals
     X1_H_k = x1
     X2_H_k = x2
-    X1_H_neg_k = x1.flip(0)
-    X2_H_neg_k = x2.flip(0)
+    X1_H_neg_k = torch.roll(x1.flip(0), shifts=1, dims=0)
+    X2_H_neg_k = torch.roll(x2.flip(0), shifts=1, dims=0)
     N = x1.size(0)
 
     result = 0.5 * (torch.einsum('bix,iox->box', X1_H_k, X2_H_k) - 
@@ -47,8 +47,10 @@ def compl_mul2d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     # Compute the DHT of both signals
     X1_H_k = x1
     X2_H_k = x2
-    X1_H_neg_k = x1.flip(0)
-    X2_H_neg_k = x2.flip(0)
+    #X1_flipped = torch.flip(x1, dims=[0, 1])
+    #X2_flipped = torch.flip(x1, dims=[0, 1])
+    X1_H_neg_k = torch.flip(x1, dims=[0, 1])
+    X2_H_neg_k = torch.flip(x1, dims=[0, 1])
     N = x1.size(0)
 
     result = 0.5 * (torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_k) - 
