303fa39c51e3dd2fc5ba6fc6ceffacee3b3f3806
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-08-25 17:58:07 -0400

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -32,9 +32,8 @@ def compl_mul1d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     # Compute the DHT of both signals
     X1_H_k = x1
     X2_H_k = x2
-    X1_H_neg_k = torch.roll(x1.flip(0), shifts=1, dims=0)
-    X2_H_neg_k = torch.roll(x2.flip(0), shifts=1, dims=0)
-    N = x1.size(0)
+    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-1]), shifts=1, dims=[-1])
+    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-1]), shifts=1, dims=[-1])
 
     result = 0.5 * (torch.einsum('bix,iox->box', X1_H_k, X2_H_k) - 
                      torch.einsum('bix,iox->box', X1_H_neg_k, X2_H_neg_k) +
@@ -47,8 +46,8 @@ def compl_mul2d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     # Compute the DHT of both signals
     X1_H_k = x1
     X2_H_k = x2
-    X1_H_neg_k = torch.roll(x1.flip(0), shifts=1, dims=0)
-    X2_H_neg_k = torch.roll(x2.flip(0), shifts=1, dims=0)
+    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-2, -1]), shifts=(1, 1), dims=[-2, -1])
+    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-2, -1]), shifts=(1, 1), dims=[-2, -1])
     N = x1.size(0)
 
     result = 0.5 * (torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_k) - 
@@ -62,8 +61,8 @@ def compl_mul3d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     # Compute the DHT of both signals
     X1_H_k = x1
     X2_H_k = x2
-    X1_H_neg_k = torch.roll(x1.flip(0), shifts=1, dims=0)
-    X2_H_neg_k = torch.roll(x2.flip(0), shifts=1, dims=0)
+    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-3, -2, -1]), shifts=(1, 1, 1), dims=[-3, -2, -1])
+    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-3, -2, -1]), shifts=(1, 1, 1), dims=[-3, -2, -1])
     N = x1.size(0)
 
     result = 0.5 * (torch.einsum('bixyz,ioxyz->boxyz', X1_H_k, X2_H_k) - 
