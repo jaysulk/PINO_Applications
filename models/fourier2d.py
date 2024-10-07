9c5880a1900b74a78a4c95b0f78c5ee4d98aa553
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-10-07 10:45:45 -0400

    Update fourier2d.py

--- models/fourier2d.py
+++ models/fourier2d.py
@@ -1,206 +1,219 @@
+import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from functools import partial
 
-from .lowrank2d import LowRank2d
-from .basics import SpectralConv2d
+def compl_mul1d(a, b):
+    # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+    return torch.einsum("bix,iox->box", a, b)
 
-class FNN2d(nn.Module):
-    def __init__(self, modes1, modes2,
-                 width=64, fc_dim=128,
-                 layers=None,
-                 in_dim=3, out_dim=1,
-                 activation='tanh',
-                 pad_x=0, pad_y=0,
-                 num_iterations=5):  # Number of von Neumann iterations
-        super(FNN2d, self).__init__()
+def compl_mul2d(a, b):
+    # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
+    return torch.einsum("bixy,ioxy->boxy", a, b)
 
+def compl_mul3d(a, b):
+    return torch.einsum("bixyz,ioxyz->boxyz", a, b)
+
+################################################################
+# 1d fourier layer
+################################################################
+
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1):
+        super(SpectralConv1d, self).__init__()
+
+        """
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes1 = modes1
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, 2))
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2])
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.in_channels, x.size(-1)//2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1] = compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+
+        # Return to physical space
+        x = torch.fft.irfft(out_ft, s=[x.size(-1)], dim=[2])
+        return x
+
+################################################################
+# 2d fourier layer
+################################################################
+
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        # Number of Fourier modes to multiply, at most floor(N/2) + 1
         self.modes1 = modes1
         self.modes2 = modes2
-        self.width = width
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.padding = (0, 0, 0, pad_y, 0, pad_x)
-        self.num_iterations = num_iterations
 
-        if layers is None:
-            self.layers = [width] * 4
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    def forward(self, x, gridy=None):
+        batchsize = x.shape[0]
+        size1 = x.shape[-2]
+        size2 = x.shape[-1]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2, 3])
+
+        if gridy is None:
+            # Multiply relevant Fourier modes
+            out_ft = torch.zeros(batchsize, self.out_channels, x.size(-2), x.size(-1) // 2 + 1, device=x.device,
+                                 dtype=torch.cfloat)
+            out_ft[:, :, :self.modes1, :self.modes2] = \
+                compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+            out_ft[:, :, -self.modes1:, :self.modes2] = \
+                compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+            # Return to physical space
+            x = torch.fft.irfftn(out_ft, s=(x.size(-2), x.size(-1)), dim=[2, 3])
         else:
-            self.layers = layers
-        self.fc0 = nn.Linear(in_dim, layers[0])
+            factor1 = compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+            factor2 = compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+            x = self.ifft2d(gridy, factor1, factor2, self.modes1, self.modes2) / (size1 * size2)
+        return x
 
-        self.sp_convs = nn.ModuleList([SpectralConv2d(
-            in_size, out_size, mode1_num, mode2_num)
-            for in_size, out_size, mode1_num, mode2_num
-            in zip(self.layers, self.layers[1:], self.modes1, self.modes2)])
+    def ifft2d(self, gridy, coeff1, coeff2, k1, k2):
+        batchsize = gridy.shape[0]
+        N = gridy.shape[1]
+        device = gridy.device
+        m1 = 2 * k1
+        m2 = 2 * k2 - 1
 
-        self.ws = nn.ModuleList([nn.Conv1d(in_size, out_size, 1)
-                                 for in_size, out_size in zip(self.layers, self.layers[1:])])
+        # wavenumber (m1, m2)
+        k_x1 =  torch.cat((torch.arange(start=0, end=k1, step=1), \
+                            torch.arange(start=-(k1), end=0, step=1)), 0).reshape(m1,1).repeat(1,m2).to(device)
+        k_x2 =  torch.cat((torch.arange(start=0, end=k2, step=1), \
+                            torch.arange(start=-(k2-1), end=0, step=1)), 0).reshape(1,m2).repeat(m1,1).to(device)
 
-        self.fc1 = nn.Linear(layers[-1], fc_dim)
-        self.fc2 = nn.Linear(fc_dim, out_dim)
+        # K = <y, k_x>,  (batch, N, m1, m2)
+        K1 = torch.outer(gridy[:,:,0].view(-1), k_x1.view(-1)).reshape(batchsize, N, m1, m2)
+        K2 = torch.outer(gridy[:,:,1].view(-1), k_x2.view(-1)).reshape(batchsize, N, m1, m2)
+        K = K1 + K2
 
-        if activation =='tanh':
-            self.activation = F.tanh
+        # basis (N, m1, m2)
+        basis = torch.exp( 1j * 2* np.pi * K).to(device)
+
+        # coeff (batch, channels, m1, m2)
+        coeff3 = coeff1[:,:,1:,1:].flip(-1, -2).conj()
+        coeff4 = torch.cat([coeff1[:,:,0:1,1:].flip(-1).conj(), coeff2[:,:,:,1:].flip(-1, -2).conj()], dim=-2)
+        coeff12 = torch.cat([coeff1, coeff2], dim=-2)
+        coeff43 = torch.cat([coeff4, coeff3], dim=-2)
+        coeff = torch.cat([coeff12, coeff43], dim=-1)
+
+        # Y (batch, channels, N)
+        Y = torch.einsum("bcxy,bnxy->bcn", coeff, basis)
+        Y = Y.real
+        return Y
+
+################################################################
+# 3d fourier layer
+################################################################
+
+class SpectralConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2, modes3):
+        super(SpectralConv3d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1  # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+        self.modes3 = modes3
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights3 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights4 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2, 3, 4])
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(2), x.size(3), x.size(4) // 2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1, :self.modes2, :self.modes3] = \
+            compl_mul3d(x_ft[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2, :self.modes3] = \
+            compl_mul3d(x_ft[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2)
+        out_ft[:, :, :self.modes1, -self.modes2:, :self.modes3] = \
+            compl_mul3d(x_ft[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3)
+        out_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3] = \
+            compl_mul3d(x_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4)
+
+        # Return to physical space
+        x = torch.fft.irfftn(out_ft, s=(x.size(2), x.size(3), x.size(4)), dim=[2, 3, 4])
+        return x
+
+################################################################
+# Fourier Block with von Neumann Operator Iteration
+################################################################
+
+class FourierBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2, modes3, activation='tanh', num_iterations=5):
+        super(FourierBlock, self).__init__()
+        self.in_channel = in_channels
+        self.out_channel = out_channels
+        self.speconv = SpectralConv3d(in_channels, out_channels, modes1, modes2, modes3)  # Spectral convolution layer
+        self.linear = nn.Conv1d(in_channels, out_channels, 1)
+        self.num_iterations = num_iterations  # Number of fixed-point iterations
+
+        # Activation function setup
+        if activation == 'tanh':
+            self.activation = torch.tanh_
         elif activation == 'gelu':
-            self.activation = F.gelu
-        elif activation == 'relu':
-            self.activation == F.relu
+            self.activation = nn.GELU()
         elif activation == 'swish':
             self.activation = self.swish
-        else:
-            raise ValueError(f'{activation} is not supported')
+        elif activation == 'none':
+            self.activation = None
 
     @staticmethod
     def swish(x):
         return x * torch.sigmoid(x)
 
     def forward(self, x):
-        length = len(self.ws)
-        batchsize = x.shape[0]
-        nx, ny = x.shape[1], x.shape[2]
-        x = F.pad(x, self.padding, "constant", 0)
-        size_x, size_y = x.shape[1], x.shape[2]
+        '''
+        input x: (batchsize, channel width, x_grid, y_grid, t_grid)
+        '''
+        # Initial state
+        u_prev = x.clone()  # u^{(n)}
 
-        x = self.fc0(x)
-        x = x.permute(0, 3, 1, 2)
-
-        # von Neumann iterations
-        u_prev = x.clone()  # Initialize with input
-
+        # Apply fixed-point iteration (von Neumann operator)
         for _ in range(self.num_iterations):
-            for i, (speconv, w) in enumerate(zip(self.sp_convs, self.ws)):
-                x1 = speconv(u_prev)
-                x2 = w(u_prev.view(batchsize, self.layers[i], -1)).view(batchsize, self.layers[i+1], size_x, size_y)
-                u_next = x1 + x2  # von Neumann update: u^{(n+1)} = \mathcal{O}(u^{(n)}) + u^{(n)}
-                if i != length - 1:
-                    u_next = self.activation(u_next)
-                u_prev = u_next  # Update the previous state
-        x = u_next.permute(0, 2, 3, 1)
-        x = self.fc1(x)
-        x = self.activation(x)
-        x = self.fc2(x)
-        x = x.reshape(batchsize, size_x, size_y, self.out_dim)
-        x = x[..., :nx, :ny, :]
-        return x
+            # Apply Fourier convolution
+            x1 = self.speconv(u_prev)
 
-class PINO2d(nn.Module):
-    def __init__(self, modes1, modes2, width, layers=None, in_dim=3, out_dim=1, num_iterations=5):
-        super(PINO2d, self).__init__()
-        self.modes1 = modes1
-        self.modes2 = modes2
-        self.width = width
-        self.num_iterations = num_iterations
+            # Apply linear layer to flattened input
+            x2 = self.linear(u_prev.view(u_prev.shape[0], self.in_channel, -1))
+            x2 = x2.view(u_prev.shape[0], self.out_channel, u_prev.shape[2], u_prev.shape[3], u_prev.shape[4])
 
-        if layers is None:
-            self.layers = [width] * 4
-        else:
-            self.layers = layers
-        self.fc0 = nn.Linear(in_dim, layers[0])
+            # Combine results
+            u_next = x1 + x2  # Update: u^{(n+1)} = \mathcal{O}(u^{(n)}) + u^{(n)}
 
-        self.sp_convs = nn.ModuleList([SpectralConv2d(
-            in_size, out_size, mode1_num, mode2_num)
-            for in_size, out_size, mode1_num, mode2_num
-            in zip(self.layers, self.layers[1:], self.modes1, self.modes2)])
+            # Apply activation if specified
+            if self.activation is not None:
+                u_next = self.activation(u_next)
 
-        self.ws = nn.ModuleList([nn.Conv1d(in_size, out_size, 1)
-                                 for in_size, out_size in zip(self.layers[:-1], self.layers[1:-1])])
-        self.ws.append(LowRank2d(self.layers[-2], self.layers[-1]))
+            # Update the previous state for the next iteration
+            u_prev = u_next
 
-        self.fc1 = nn.Linear(layers[-1], layers[-1] * 4)
-        self.fc2 = nn.Linear(layers[-1] * 4, out_dim)
-
-    def forward(self, x, y=None):
-        batchsize = x.shape[0]
-        size_x, size_y = x.shape[1], x.shape[2]
-
-        length = len(self.ws)
-        x = self.fc0(x)
-        x = x.permute(0, 3, 1, 2)
-
-        # von Neumann iterations
-        u_prev = x.clone()
-
-        for _ in range(self.num_iterations):
-            for i, (speconv, w) in enumerate(zip(self.sp_convs, self.ws)):
-                if i != length - 1:
-                    x1 = speconv(u_prev)
-                    x2 = w(u_prev.view(batchsize, self.layers[i], -1))\
-                        .view(batchsize, self.layers[i+1], size_x, size_y)
-                    u_next = x1 + x2
-                    u_next = F.selu(u_next)
-                else:
-                    x1 = speconv(u_prev, y).reshape(batchsize, self.layers[-1], -1)
-                    x2 = w(u_prev, y).reshape(batchsize, self.layers[-1], -1)
-                    u_next = x1 + x2
-                u_prev = u_next
-        x = u_next.permute(0, 2, 1)
-        x = self.fc1(x)
-        x = F.selu(x)
-        x = self.fc2(x)
-        return x
-
-class FNN2d_AD(nn.Module):
-    def __init__(self, modes1, modes2,
-                 width=64, fc_dim=128,
-                 layers=None,
-                 in_dim=3, out_dim=1,
-                 activation='tanh',
-                 num_iterations=5):
-        super(FNN2d_AD, self).__init__()
-
-        self.modes1 = modes1
-        self.modes2 = modes2
-        self.width = width
-        self.num_iterations = num_iterations
-
-        if layers is None:
-            self.layers = [width] * 4
-        else:
-            self.layers = layers
-        self.fc0 = nn.Linear(in_dim, layers[0])
-
-        self.sp_convs = nn.ModuleList([SpectralConv2d(
-            in_size, out_size, mode1_num, mode2_num)
-            for in_size, out_size, mode1_num, mode2_num
-            in zip(self.layers, self.layers[1:], self.modes1, self.modes2)])
-
-        self.ws = nn.ModuleList([nn.Conv1d(in_size, out_size, 1)
-                                 for in_size, out_size in zip(self.layers[:-1], self.layers[1:-1])])
-        self.ws.append(LowRank2d(self.layers[-2], self.layers[-1]))
-
-        self.fc1 = nn.Linear(layers[-1], fc_dim)
-        self.fc2 = nn.Linear(fc_dim, out_dim)
-
-        if activation =='tanh':
-            self.activation = F.tanh
-        elif activation == 'gelu':
-            self.activation = F.gelu
-        elif activation == 'relu':
-            self.activation = F.relu
-        elif activation == 'swish':
-            self.activation = self.swish
-        else:
-            raise ValueError(f'{activation} is not supported')
-
-    @staticmethod
-    def swish(x):
-        return x * torch.sigmoid(x)
-
-    def forward(self, x, y=None):
-        length = len(self.ws)
-        batchsize = x.shape[0]
-        size_x, size_y = x.shape[1], x.shape[2]
-
-        x = self.fc0(x)
-        x = x.permute(0, 3, 1, 2)
-
-        # von Neumann iterations
-        u_prev = x.clone()
-
-        for _ in range(self.num_iterations):
-            for i, (speconv, w) in enumerate(zip(self.sp_convs, self.ws)):
-                if i != length - 1:
-                    x1 = speconv(u_prev)
-                    x2 = w(u_prev.view(batchsize, self.layers[i], -1)).view(batchsize, self.layers[i+1], size_x, size_y)
-                    u_next = x1 +
+        # Return the final result after all iterations
+        return u_next
