61850efeb0f829ebce1404c41fe56f37be2fe3bc
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-10-09 15:01:13 -0400

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -183,50 +183,6 @@ def compl_mul3d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
 #    
 #    return augmented_inputs
 
-def compl_mul1d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-    # Compute the DHT of both signals
-    X1_H_k = x1
-    X2_H_k = x2
-    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-1]), shifts=1, dims=[-1])
-    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-1]), shifts=1, dims=[-1])
-
-    result = 0.5 * (torch.einsum('bix,iox->box', X1_H_k, X2_H_k) - 
-                     torch.einsum('bix,iox->box', X1_H_neg_k, X2_H_neg_k) +
-                     torch.einsum('bix,iox->box', X1_H_k, X2_H_neg_k) + 
-                     torch.einsum('bix,iox->box', X1_H_neg_k, X2_H_k))
-
-    return result
-
-def compl_mul2d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-    # Compute the DHT of both signals
-    X1_H_k = x1
-    X2_H_k = x2
-    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-1, -2]), shifts=(1, 1), dims=[-1, -2])
-    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-1, -2]), shifts=(1, 1), dims=[-1, -2])
-    
-    # Perform the convolution using DHT components
-    result = 0.5 * (torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_k) - 
-                    torch.einsum('bixy,ioxy->boxy', X1_H_neg_k, X2_H_neg_k) +
-                    torch.einsum('bixy,ioxy->boxy', X1_H_k, X2_H_neg_k) + 
-                    torch.einsum('bixy,ioxy->boxy', X1_H_neg_k, X2_H_k))
-    
-    return result
-
-    
-def compl_mul3d(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-    # Compute the DHT of both signals
-    X1_H_k = x1
-    X2_H_k = x2
-    X1_H_neg_k = torch.roll(torch.flip(x1, dims=[-3, -2, -1]), shifts=(1, 1, 1), dims=[-3, -2, -1])
-    X2_H_neg_k = torch.roll(torch.flip(x2, dims=[-3, -2, -1]), shifts=(1, 1, 1), dims=[-3, -2, -1])
-
-    result = 0.5 * (torch.einsum('bixyz,ioxyz->boxyz', X1_H_k, X2_H_k) - 
-                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_neg_k, X2_H_neg_k) +
-                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_k, X2_H_neg_k) + 
-                     torch.einsum('bixyz,ioxyz->boxyz', X1_H_neg_k, X2_H_k))
-
-    return result
-
 ################################################################
 # Low-Pass Filter Function
 ################################################################
