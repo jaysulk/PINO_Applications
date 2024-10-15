fa7e2a16180e147c2ebcc9a044072fd3e75e4b87
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-10-15 14:45:22 -0400

    Update basics.py

--- models/basics.py
+++ models/basics.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import torch
-
 # Define the DHT and IDHT functions with corrections
 def dht(x: torch.Tensor, dim=None) -> torch.Tensor:
     result = torch.fft.fftn(x, dim=dim, norm='ortho')
